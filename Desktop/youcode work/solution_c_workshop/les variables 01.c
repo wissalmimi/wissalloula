890ecb517990a1effb81d7894d0e1dab7a1284c7
wissal boutrid <wissalasy24@gmail.com>
2024-09-26 18:08:40 +0200

    wissal

---
+++ Desktop/youcode work/solution_c_workshop/les variables 01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char name[50],last_name[50],gender[50],email[50];
+    int age;
+ 
+    printf("first name:\n");
+    scanf("%s",&name);
+
+    printf("give me your last name:\n");
+    scanf("%s",&last_name);
+ 
+    printf("what is your ginder: \n");
+    scanf("%s",&gender);
+ 
+ 
+    printf("your email adresse:\n");
+    scanf("%s",&email);
+  
+    printf("how old are you : \n");
+    scanf("%d",&age);
+ 
+}
+Envoyer un message à @Protocol
